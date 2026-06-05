a025a33b91f2bc2a4ddc469e7cd6a8b465a6989e
davinbarron <davinbarron667@gmail.com>
2026-06-05 22:09:00 +0100

    feat: log loader class

---
+++ src/log_loader.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import polars as pl
+
+class LogLoader:
+    def __init__(self, paths):
+        self._paths = paths
+
+    @classmethod
+    def from_path(cls, path):
+        file_path = Path(path)
+        
+        if file_path.is_dir():
+            return cls(list(file_path.glob("*.json")))
+            
+        return cls([file_path])
+
+    def load(self):
+        return pl.read_ndjson(self._paths)
+
+    
+if __name__ == '__main__':
+    file_loader = LogLoader.from_path("data/logs/2021-04-061000.json")
+    dir_loader = LogLoader.from_path("data/logs")
+    
+    df = file_loader.load()
+    df2 = dir_loader.load()
+
+    print(df.head())
+    print(df2.head())
