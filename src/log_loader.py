00601f0d10fe5524657b380baf2d884a6eba02f4
davinbarron <davinbarron667@gmail.com>
2026-06-08 14:24:09 +0100

    feat: added a schema parameter to the load method in the log loader class

--- src/log_loader.py
+++ src/log_loader.py
@@ -1,14 +1,18 @@
 import polars as pl
 from pathlib import Path
 
+
 class LogLoaderError(Exception):
     pass
+
 
 class LogFileNotFoundError(LogLoaderError):
     pass
 
+
 class LogLoadingError(LogLoaderError):
     pass
+
 
 class LogLoader:
     def __init__(self, paths):
@@ -20,34 +24,44 @@ class LogLoader:
 
         if not file_path.exists():
             raise LogFileNotFoundError(f"The path '{path}' does not exist.")
-        
+
         if file_path.is_dir():
             json_files = list(file_path.glob("*.json"))
-            
+
             if not json_files:
-                raise LogFileNotFoundError(f"No '.json' files found in directory '{path}'.")
-            
+                raise LogFileNotFoundError(
+                    f"No '.json' files found in directory '{path}'."
+                )
+
             return cls(json_files)
-            
+
         return cls([file_path])
-    
+
     @property
     def paths(self):
         return self._paths
 
-    def load(self):
+    def load(self, schema=None):
         try:
-            return pl.read_ndjson(self.paths)
+            return pl.read_ndjson(self.paths, schema=schema)
         except Exception as e:
             raise LogLoadingError(f"Failed to load log data: {e}")
 
-    
-if __name__ == '__main__':
-    file_loader = LogLoader.from_path("data/logs/2021-04-061000.json")
-    dir_loader = LogLoader.from_path("data/logs")
-    
-    df = file_loader.load()
-    df2 = dir_loader.load()
+
+if __name__ == "__main__":
+    # file_loader = LogLoader.from_path("data/logs/2021-04-061000.json")
+    # dir_loader = LogLoader.from_path("data/logs")
+
+    # df = file_loader.load()
+    # df2 = dir_loader.load()
+
+    # print(df.head())
+    # print(df2.head())
+
+    from yaml_loader import YamlLoader
+
+    schema = YamlLoader.from_path("configs/schema_metadata.yaml").get_schema()
+    log_loader = LogLoader.from_path("data/logs")
+    df = log_loader.load(schema)
 
     print(df.head())
-    print(df2.head())
